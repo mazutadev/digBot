abf197afda9c75252762d564bcfab4d0c020982b
mazuta <mazuta.dev@gmail.com>
2023-06-01 18:09:19 +0300

    Delite not use logic in utils.position.lua

--- src/utils/position.lua
+++ src/utils/position.lua
@@ -2,7 +2,6 @@
 local check = require("utils.check")
 
 local function record_position_y(current_x, current_z)
-
     if check.even(current_z) then
         if check.even(current_x) then
             return 1
@@ -16,34 +15,11 @@ local function record_position_y(current_x, current_z)
             return 1
         end
     end
-
-    if check.even(input_width) then
-        if check.even(current_x) then
-            return current_y - 1
-        else
-            return current_y + 1
-        end
-    else
-        if check.even(current_z) then
-            if check.even(current_x) then
-                return current_y + 1
-            else
-                return current_y - 1
-            end
-        else
-            if check.even(current_x) then
-                return current_y - 1
-            else
-                return current_y + 1
-            end
-        end
-    end
 end
 
 local function record_position_x(current_z)
     return check.even(current_z) and - 1 or 1
 end
-
 
 return {
     record_y = record_position_y,
