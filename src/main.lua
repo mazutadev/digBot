e750855519ffb84e0137ca7ebdc386cd6dfd22cd
mazuta <mazuta.dev@gmail.com>
2023-06-01 04:00:11 +0300

    Refactored main.lua var current_y and utils/position.lua record_position_y()

--- src/main.lua
+++ src/main.lua
@@ -31,7 +31,6 @@ turtle.digDown()
 turtle.down()
 
 local current_x, current_y, current_z = 1, 1, 1
-local width_position, length_position, height_position = 1, 1, 1
 
 term.clear()
 -- BEGIN DIG TURTLE HEIGHT
@@ -45,13 +44,12 @@ for i = 1, height do
         for k = 1, length -1 do
             utils.dig.forward()
             turtle.forward()
-            current_y = utils.position.record_y(width_position, height_position, width, current_y)
-            length_position = length_position + 1
+            current_y = current_y + utils.position.record_y(current_x, current_y)
             utils.output.current_position(current_y, current_x, current_z)
         end
         -- END DIG TURTLE LENGTH
 
-        if width_position == width then -- TODO: Change to current_x logic
+        if width_position == width then -- TODO: Change to current_x stop logic
             break
         end
         utils.move.set_direction(current_x)
@@ -60,17 +58,17 @@ for i = 1, height do
     -- END DIG TURTLE WIDTH
     end
 
-    if height_position == height then
+    if current_z == height then -- TODO: Change to current_y stop logic
         break
+    else
+        turtle.digDown()
+        turtle.down()
+        turtle.turnRight()
+        turtle.turnRight()
+
+        current_z = current_z + 1
     end
 
-    turtle.digDown()
-    turtle.down()
-    turtle.turnRight()
-    turtle.turnRight()
-    height_position = height_position + 1
-    current_z = utils.position.record_z(height_position, height)
-    length_position, width_position = 1, 1
 -- END DIG TURTLE HEIGHT
 end
 
--- src/utils/position.lua
+++ src/utils/position.lua
@@ -1,23 +1,37 @@
 
 local check = require("utils.check")
 
-local function record_position_y(width_position, height_position, input_width, current_y)
+local function record_position_y(current_x, current_z)
+
+    if check.even(current_z) then
+        if check.even(current_x) then
+            return 1
+        else
+            return - 1
+        end
+    else
+        if check.even(current_x) then
+            return - 1
+        else
+            return 1
+        end
+    end
 
     if check.even(input_width) then
-        if check.even(width_position) then
+        if check.even(current_x) then
             return current_y - 1
         else
             return current_y + 1
         end
     else
-        if check.even(height_position) then
-            if check.even(width_position) then
+        if check.even(current_z) then
+            if check.even(current_x) then
                 return current_y + 1
             else
                 return current_y - 1
             end
         else
-            if check.even(width_position) then
+            if check.even(current_x) then
                 return current_y - 1
             else
                 return current_y + 1
@@ -30,16 +44,8 @@ local function record_position_x(current_z)
     return check.even(current_z) and - 1 or 1
 end
 
-local function record_position_z(height_position, input_height)
-    if height_position == input_height then
-        return height_position
-    else
-        return height_position + 1
-    end
-end
 
 return {
     record_y = record_position_y,
     record_x = record_position_x,
-    record_z = record_position_z
 }
